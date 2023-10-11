b7c311968a14cd8a083a6f9942fa746b4cd05a2d
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2023-10-11 16:55:51 -0400

    Working on the frontend

--- includes/main-hero-animation.php
+++ includes/main-hero-animation.php
@@ -25,14 +25,3 @@
         endif; ?>
     </div>
 </div>
-
-<script>
-    const services = document.querySelector('.hero-animation-services');
-    const totalServices = services.children.length;
-
-    for (let i = 0; i < totalServices; i++) {
-        services.appendChild(services.children[i].cloneNode(true));
-    }
-
-    document.documentElement.style.setProperty('--total-services', totalServices);
-</script>
--- js/orb-services-hero.js
+++ js/orb-services-hero.js
@@ -1,43 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const container = document.querySelector('#hero-animation-services');
-    const items = container.querySelectorAll('#hero-animation-service');
-    const length = items.length;
+const services = document.querySelector('.hero-animation-services');
+const totalServices = services.children.length;
 
-    let keyframes = '';
+for (let i = 0; i < totalServices; i++) {
+    services.appendChild(services.children[i].cloneNode(true));
+}
 
-    for (let i = 0; i < length; i++) {
-        const offset = -(i * 100);
-        const multiple = length - 1;
-        const percentage = 100 / multiple;
-
-        keyframes += `
-        ${i * percentage}% {
-        transform: translateY(${offset}%);
-        }
-    `;
-    }
-
-    function getStyleSheetByName(name) {
-        const styleSheets = document.styleSheets;
-
-        for (let i = 0; i < styleSheets.length; i++) {
-            const styleSheet = styleSheets[i];
-
-            if (styleSheet.ownerNode.id === name) {
-                
-                return styleSheet;
-            }
-        }
-
-        return null;
-    }
-
-    const targetStyleSheet = getStyleSheetByName("orb-services-css");
-
-    if (targetStyleSheet) {
-        targetStyleSheet.insertRule(`@keyframes cycle { ${keyframes} }`, targetStyleSheet.cssRules.length);
-    } else {
-        console.log("Stylesheet not found");
-    }
-    document.documentElement.style.setProperty('--animation-duration', `${length}s`);
-});
+document.documentElement.style.setProperty('--total-services', totalServices);
