33055330ee3c2dffe4884fec0fe2a7cb643d95c8
Jamel Lyons <32249473+the7ofdiamonds@users.noreply.github.com>
2023-11-11 00:59:02 -0500

    Refactoring css loads when needed

--- CSS/CSS.php
+++ CSS/CSS.php
@@ -4,6 +4,7 @@ namespace ORB\Products_Services\CSS;
 
 use ORB\Products_Services\Pages\Pages;
 use ORB\Products_Services\Post_Types\Post_Types;
+
 use ORB\Products_Services\CSS\Customizer\Customizer;
 
 class CSS
@@ -44,7 +45,7 @@ class CSS
 
     function load_front_page_css()
     {
-        if (is_front_page()) {
+        if ($_SERVER['REQUEST_URI'] === '/') {
             if ($this->filePath) {
                 wp_register_style($this->handle_prefix . 'css',  $this->cssFolderPathURL . $this->cssFileName, array(), false, 'all');
                 wp_enqueue_style($this->handle_prefix . 'css');
@@ -57,7 +58,23 @@ class CSS
     function load_pages_css()
     {
         foreach ($this->page_titles as $page) {
-            if (is_page($page)) {
+            $full_url = explode('/', $page);
+            $full_path = explode('/', $_SERVER['REQUEST_URI']);
+
+            $full_url = array_filter($full_url, function ($value) {
+                return $value !== "";
+            });
+
+            $full_path = array_filter($full_path, function ($value) {
+                return $value !== "";
+            });
+
+            $full_url = array_values($full_url);
+            $full_path = array_values($full_path);
+
+            $differences = array_diff($full_url, $full_path);
+
+            if (empty($differences)) {
                 if ($this->filePath) {
                     wp_register_style($this->handle_prefix . 'css',  $this->cssFolderPathURL . $this->cssFileName, array(), false, 'all');
                     wp_enqueue_style($this->handle_prefix . 'css');
